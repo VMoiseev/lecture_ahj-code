5e4d8e4e7971c6e41c11a9749a729adbd05744b7
VMoiseev <e175@inbox.ru>
2023-01-22 23:41:39 +0500

    fix

--- 5.forms/src/js/app.js
+++ 5.forms/src/js/app.js
@@ -27,21 +27,24 @@ form.addEventListener('submit', (e) => {
   const { elements } = form;
   // const elements = form.elements; тоже, что и 27строка
 
-  // eslint-disable-next-line array-callback-return
-  const invalid = [...elements].some((el) => Object.keys(ValidityState.prototype).some((key) => {
-    if (!el.name) return;
-    if (key === 'valid') return;
+  // eslint-disable-next-line arrow-body-style
+  const invalid = [...elements].some((el) => {
+    // eslint-disable-next-line array-callback-return
+    return Object.keys(ValidityState.prototype).some((key) => {
+      if (!el.name) return;
+      if (key === 'valid') return;
 
-    if (el.validity[key]) {
-      console.log(key);
-      console.log(errors[el.name][key]);
+      if (el.validity[key]) {
+        console.log(key);
+        console.log(errors[el.name][key]);
 
-      el.setCustomValidity(errors[el.name][key]);
+        el.setCustomValidity(errors[el.name][key]);
 
-      // eslint-disable-next-line consistent-return
-      return true;
-    }
-  }));
+        // eslint-disable-next-line consistent-return
+        return true;
+      }
+    });
+  });
 
   if (invalid) {
     form.reportValidity();
